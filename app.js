225f3221bbf2b9d469a9ac064253f301e6bc4c95
Ronnit3012 <ronnit.mirgh@gmail.com>
2021-11-23 17:33:46 +0530

    Delete & Filter Tasks

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ LoadEventListeners();
 function LoadEventListeners() {          // to call an event whenever Add Task button has been clicked
     // Add task event
     form.addEventListener('submit', addTask);
+
+    // Remove task event
+    taskList.addEventListener('click', removeTask);
+
+    // Clear task event
+    clearBtn.addEventListener('click', clearTasks);
+
+    // Filter task event
+    filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -50,9 +59,40 @@ function addTask(e) {
     e.preventDefault();
 }
 
+// Remove Task
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item')) {
+        if(confirm('Are You Sure?')) {        // it will ask for confirmation every time we click on the cross
+            e.target.parentElement.parentElement.remove();         // it will remove the task
+        }
+    }
+    // console.log(e.target);
 
+    e.preventDefault();
+}
 
+// Clear Tasks
+function clearTasks() {
+    // taskList.innerHTML = '';
 
+    // Faster 
+    while(taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);           // this will remove all the tasks from the task list
+    }
 
+    // https://jsperf.com/innerhtml-vs-removechild     // Difference between directly clearing of the lists and looping on each list to be cleared
+}
 
+// Filter Tasks          // it will search for the task with every letter entered in the filter input field
+function filterTasks() {
+    const text = e.target.value.toLowerCase();     // it will take the value from the input field of the filter text and convert it into lower case and store it in const text
 
+    document.querySelectorAll('.collection-item').forEach(function(task) {
+        const item = task.firstChild.textContent;                  // it will store the current text in the Input field
+        if(item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';       // it will show the matches 
+        } else {
+            task.style.display = 'none';       // it will show nothing if the text doesnt matches with any task
+        }
+    });
+}
